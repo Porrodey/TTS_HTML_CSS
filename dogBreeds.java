74df104060db9bb83a30c204c6842c93322cd625
Porrodey <73514219+Porrodey@users.noreply.github.com>
2020-11-27 00:00:18 -0500

    Java-TypesVariablesOperator
    Java program created in Eclipse that includes errors in field and variable names and uninitialized field.

---
+++ dogBreeds.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+public class #dogBreeds {
+	
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//System.out.print("Hello, what kind of dog do you have?\n");
+		//System.out.print("Dogs are the best!");
+
+		System.out.println("What's your favorite dog breed?");
+        String dogBreed = input.nextLine();
+        System.out.printf("Wow! %s!\n are very cute!", dogBreed);
+        System.out.println("All dog breeds are great because they are all dogs and I love dogs!");
+        
+        
+        if (choice.equals("big") || choice.equals("large")) {
+            System.out.println("Those are my absolute favorite kinds of dogs.");
+        } else {
+            System.out.println("Small dogs are cute, but they are not my favorite.");
+        }
+
+       
+        System.out.println("How much does your dog weigh?");
+        
+       int #weight;
+        
+        if (weight < weightLimit){
+        	  System.out.println("Your dog is too small to go to the dog park.");
+        	  System.exit(0);
+        	} else {
+        	   System.out.println("That means we can go to the dog park!")
+        	}
+}
+
+	}
+
+
